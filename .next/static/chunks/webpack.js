296c7f1db47771771e82537571c17e9d76fbecf7
eldoraboo <eldoraboo.mby@gmail.com>
2023-02-01 14:16:25 +0800

    bug: optimise keyboard

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90a695e529ed639d"; }
+/******/ 		__webpack_require__.h = function() { return "c3d4c28df3c772c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/second.tsx
+++ pages/second.tsx
@@ -70,28 +70,28 @@ export default function PianoKeyboard(props: any) {
   const notes = [
     { key: 0, value: "C" },
     { key: 1, value: "D" },
-    { key: 0.5, left: "135%", black: true, value: "C♯/D♭" },
+    { key: 0.5, left: "6.7vw", black: true, value: "C♯/D♭" },
     { key: 2, left: "100%", value: "E" },
-    { key: 1.5, left: "235%", black: true, value: "D♯/E♭" },
+    { key: 1.5, left: "11.7vw", black: true, value: "D♯/E♭" },
     { key: 2.5, left: "200%", value: "F" },
     { key: 3.5, left: "200%", value: "G" },
-    { key: 3, left: "335%", black: true, value: "F♯/G♭" },
+    { key: 3, left: "16.7vw", black: true, value: "F♯/G♭" },
     { key: 4.5, left: "300%", value: "A" },
-    { key: 4, left: "435%", black: true, value: "G♯/A♭" },
+    { key: 4, left: "21.7vw", black: true, value: "G♯/A♭" },
     { key: 5.5, left: "400%", value: "B" },
-    { key: 5, left: "535%", black: true, value: "A♯/B♭" },
+    { key: 5, left: "26.7vw", black: true, value: "A♯/B♭" },
     { key: 6, left: "500%", value: "C" },
     { key: 7, left: "500%", value: "D" },
-    { key: 6.5, left: "635%", black: true, value: "C♯/D♭" },
+    { key: 6.5, left: "31.7vw", black: true, value: "C♯/D♭" },
     { key: 8, left: "600%", value: "E" },
-    { key: 7.5, left: "735%", black: true, value: "D♯/E♭" },
+    { key: 7.5, left: "36.7vw", black: true, value: "D♯/E♭" },
     { key: 8.5, left: "700%", value: "F" },
     { key: 9.5, left: "700%", value: "G" },
-    { key: 9, left: "835%", black: true, value: "F♯/G♭" },
+    { key: 9, left: "41.7vw", black: true, value: "F♯/G♭" },
     { key: 10.5, left: "800%", value: "A" },
-    { key: 10, left: "935%", black: true, value: "G♯/A♭" },
+    { key: 10, left: "46.7vw", black: true, value: "G♯/A♭" },
     { key: 11.5, left: "900%", value: "B" },
-    { key: 11, left: "1035%", black: true, value: "A♯/B♭" },
+    { key: 11, left: "51.7vw", black: true, value: "A♯/B♭" },
     { key: 12, left: "1000%", value: "C" },
   ];
 
@@ -99,7 +99,7 @@ export default function PianoKeyboard(props: any) {
     <SimpleGrid
       px={12}
       height="20vw"
-      templateColumns="repeat(25, minmax(6.65%, 1fr))"
+      templateColumns="repeat(25, minmax(5vw, 1fr))"
       mb={10}
     >
       {notes.map((note) => (
@@ -108,7 +108,7 @@ export default function PianoKeyboard(props: any) {
           key={note.key}
           height={note.black ? "12vw" : "20vw"}
           variant={note.black ? "filled" : "outline"}
-          width={note.black ? "70%" : "100%"}
+          width={note.black ? "3.8vw" : "5vw"}
           pt={note.black ? "8vw" : "10vw"}
           right={note.left}
           fontSize={note.black ? "0.5em" : "1em"}
